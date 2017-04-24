5547753afa8ffd51f28094439faf1f29b3b8f9e6
RushanS <seif.rushan@gmail.ru>
2017-04-25 00:46:09 +0300

    add signal handle

--- myshell.c
+++ myshell.c
@@ -3,14 +3,25 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+pid_t child_pid;
+
+void kill_process(int sign)
+{
+	kill(child_pid, sign);
+}
 
 void main()
 {
 	pid_t pid;
+	int status;
+	pid_t child_pid;
 	char *words[100];
 	char str[100];
 	char sep[] = " ";
-	
+
 	while(1) {
 		printf("> ");		
 		gets(str);
@@ -32,6 +43,11 @@ void main()
 		
 		if (strcmp(words[0], "exit") == 0)
 			exit(0);
+			
+		if (strcmp(words[0], "cd") == 0) {
+			chdir(words[1]);
+			continue;
+		}
 		
 		pid = fork();
 		switch(pid)
@@ -40,10 +56,20 @@ void main()
 				perror("fork failed");
 				exit(1);
 			case 0:
+				(void) signal(SIGINT, SIG_DFL);
 				execvp(words[0], words);
 			default:
-				if (mode)
-					wait();
+				if (mode) {
+					child_pid = pid;
+					(void) signal(SIGINT, SIG_IGN);
+//					(void) signal(SIGINT, kill_process);
+					child_pid = wait(&status);
+					if (WIFEXITED(status))
+						printf("------ process finished ------\n");
+					else printf("------ process interrupted -----\n");
+				} else {
+					(void) signal(SIGINT, SIG_DFL);
+				}
 		}
 	}
 }
